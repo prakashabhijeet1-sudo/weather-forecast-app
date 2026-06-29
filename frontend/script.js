1fa9adce5e6d6d66afcd0a98d0dff0765354596c
prakashabhijeet1-sudo <prakashabhijeet1@gmail.com>
2026-06-29 12:49:38 +0530

    required changes done

--- frontend/script.js
+++ frontend/script.js
@@ -1,18 +1,12 @@
 async function getWeather() {
-
     const city = document.getElementById("city").value;
-
     const response = await fetch(`http://localhost:5000/weather/${city}`);
-
     const data = await response.json();
-
     const resultDiv = document.getElementById("weatherResult");
-
     if(data.error){
         resultDiv.innerHTML = `<p>${data.error}</p>`;
         return;
     }
-
     resultDiv.innerHTML = `
         <h2>${data.city}</h2>
         <p>Temperature: ${data.temperature} °C</p>
